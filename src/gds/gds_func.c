d07debbd0c7c84e2280c16016ac8fcebd41a6201
emiyl <me@emiyl.com>
2026-09-25 21:11:20 +0100

    fix

--- src/gds/gds_func.c
+++ src/gds/gds_func.c
@@ -24,7 +24,7 @@ static bool gds_func_SetMap(gds_reader_t *reader, const gds_record_t *command,
         impl->setmap(impl, map_text_id, map_background_id, param3, param4,
                      param5);
     else {
-        fprintf(stderr, "widebrim: setmap function pointer is NULL - is this "
+        fprintf(stderr, "gds: setmap function pointer is NULL - is this "
                         "called from a room?\n");
     }
 
